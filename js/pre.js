549dd805011af68471c7c828d1397bbdb31c903e
Moti Zilberman <motiz88@gmail.com>
2015-12-01 19:58:09 +0200

    (js) Some test code in pre.js

--- js/pre.js
+++ js/pre.js
@@ -1,5 +1,7 @@
 if (typeof(Module) === "undefined") Module = {};
 Module["arguments"] = ['--run', 'script.praat'];
 Module["preRun"] = function() {
-	FS.writeFile('script.praat', "writeInfoLine: \"Yo yo\"\n");
+	FS.writeFile('script.praat', ['Create Sound as pure tone: "tone", 1, 0, 0.4, 44100, 440, 0.2, 0.01, 0.01',
+'To Pitch: 0, 75, 600',
+'Get mean: 0, 0, "Hertz"'].join('\n'));
 };
